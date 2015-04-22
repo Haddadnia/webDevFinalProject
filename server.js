c5e00c7634245a81823544883773e103054c4367
mbentson <bentson.m@husky.neu.edu>
2015-04-22 16:57:24 -0400

    testing search on openshift take 2

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
 //var textSearch = require('mongoose-text-search');
 // default to a 'localhost' configuration:
-var connectionString = 'mongodb://localhost/test';
+var connectionString = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/test';
 // if OPENSHIFT env variables are present, use the available connection info:
+/*
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
@@ -10,6 +11,7 @@ if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
     process.env.OPENSHIFT_APP_NAME;
 }
+*/
 var db = mongoose.connect(connectionString);
 var express = require('express');
 var app = express();
